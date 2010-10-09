4df759e66cf11c940707bf79809fb28f3192b51b
Rowan James <rowanj@phere.net>
2010-10-09 17:39:56 +1100

    Remove commented-out block

--- Dot3Light.mm
+++ Dot3Light.mm
@@ -58,51 +58,5 @@ ImagePixel Dot3Light::CalculateOutput(const Image& oSource, const int x, const i
 	float fLit = Math::MapRange(0.0f, 1.0f, 0.0f, 255.0f, fDot);
 	output.r = output.g = output.b = fLit;
 
-	
-
-	/*
-	if (output.a == 0) {
-		
-		// find all neighboring non-transparent pixels
-		list<pair<ImagePixel, float> > blendWith = FindClosest(oSource, x, y);
-		float fCount = blendWith.size();
-		
-		float fTotalDistance(0.0);
-		float fMaximumDistance(0.0);
-		for (list<pair<ImagePixel, float> >::iterator it(blendWith.begin());
-			 it != blendWith.end(); ++it) {
-			fTotalDistance += it->second;
-			fMaximumDistance = max(fMaximumDistance, it->second);
-		}
-		float fAverageDistance(fTotalDistance/fCount);
-		
-		
-		double dR(0.0);
-		double dG(0.0);
-		double dB(0.0);
-		float fTotalWeight(0.0);
-		for (list<pair<ImagePixel, float> >::iterator it(blendWith.begin());
-			 it != blendWith.end(); ++it) {
-			float fDistance(it->second);
-			float fWeight = fAverageDistance / fDistance;
-			//			fWeight = 1.0;
-			// accumulate as though range was -1 to 1, * weight
-			fTotalWeight += fWeight;
-			dR += (((it->first.r / 255.0) * 2.0) - 1.0) * fWeight;
-			dG += (((it->first.g / 255.0) * 2.0) - 1.0) * fWeight;
-			dB += (((it->first.b / 255.0) * 2.0) - 1.0) * fWeight;
-		}
-		
-		if (fCount > 0.0) {
-			output.r = max(min(255.0, 128 + (dR * 128.0) / fTotalWeight), 0.0);
-			output.g = max(min(255.0, 128 + (dG * 128.0) / fTotalWeight), 0.0);
-			output.b = max(min(255.0, 128 + (dB * 128.0) / fTotalWeight), 0.0);
-		} else {
-			output.r = 128;
-			output.g = 128;
-			output.b = 255;
-		}
-	}
-	output.a = 255; */
 	return output;
 }
